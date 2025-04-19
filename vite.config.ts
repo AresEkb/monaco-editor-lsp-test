0d150cec4972762c080257751f045b8ed2c33f71
Denis Nikiforov <denis.nikif@gmail.com>
2025-04-20 00:31:36 +0500

    Update README.md

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022',
+  },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [importMetaUrlPlugin]
     }
+  },
+  worker: {
+    format: 'es'
   }
 })
